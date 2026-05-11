4ca8be43a9166b005181b859388840dd70c0e1f8
HannaQahoush <hanna.m.qahoush@gmail.com>
2026-05-11 18:12:48 +0300

    Update RR.cpp

--- marwan/RR.cpp
+++ marwan/RR.cpp
@@ -101,6 +101,8 @@ public:
     }
 };
 
+// this function (printGanttChart) was implemented using AI enhanced tools for learning purposes.
+// It is only used for displaying the Gantt chart output and does not perform any scheduling calculations or computations.
 void printGanttChart(const vector<pair<string, pair<int, int>>>& segments)
 {
     cout << "\nGantt Chart:\n";
